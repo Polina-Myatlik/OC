caa2b63192f733b8468f4802deaf88995b537377
Polina-Myatlik <121228435+Polina-Myatlik@users.noreply.github.com>
2023-02-27 09:39:44 +0300

    Add files via upload

---
+++ 1. Кнопочка/Кнопочка/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Кнопочка
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button2.Text = "Пришёл";
+            button1.Text = "Ушёл";
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Text = "Пришёл";
+            button2.Text = "Ушёл";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = textBox1.Text;
+                double number = double.Parse(s);
+                number *= 2;
+                textBox1.Text = number.ToString();
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = textBox1.Text;
+                double number = double.Parse(s);
+                number /= 2;
+                textBox1.Text = number.ToString();
+            }
+            catch { }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = textBox1.Text;
+                double number = double.Parse(s);
+                if (number == 0)
+                {
+                    textBox1.BackColor = Color.Red;
+                }
+                else
+                {
+                    textBox1.BackColor = Color.LightSkyBlue;
+                }
+            }
+            catch { }
+        }
+    }
+}
---
+++ 2. Диск/Диск/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Диск
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 250;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                label2.Text = $"Наличие диска: {drive.IsReady}\nНазвание: {drive.Name}";
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (DriveInfo drive in drives)
+                {
+                    label1.Text = $"Кол-во свободного места: {drive.AvailableFreeSpace} байт\nОбъём памяти: {drive.TotalSize} байт";
+                }
+            }
+        }
+    }
+}
