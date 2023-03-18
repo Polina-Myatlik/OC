571930a670cc6010292510ddbcfcdc9defb61f00
Polina-Myatlik <121228435+Polina-Myatlik@users.noreply.github.com>
2023-03-18 23:41:20 +0300

    Add files via upload

---
+++ 5. dll/5. dll/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5.dll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count1 = 0;
+        int count2 = 0;
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            count1++;
+            int a = Convert.ToInt32(textBox3.Text);
+            int b = Convert.ToInt32(textBox3.Text);
+            string s = textBox1.Text;
+            string res = Lib.Class1.Number(s, a, b+1);
+            textBox2.Text = res;
+            if(res.Contains("Загаданное число:") == false)
+            {
+                count2++;
+            }
+            label1.Text = $"Кол-во побед: {count2} Кол-во игор: {count1}";
+        }
+    }
+}
---
+++ 5. dll/5. dll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.dll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+        }
+    }
+}
