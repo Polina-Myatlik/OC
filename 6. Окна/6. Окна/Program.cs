4f59f14e52bf8fe9cd4215e33c03282e5ddc6471
Polina-Myatlik <121228435+Polina-Myatlik@users.noreply.github.com>
2023-03-28 00:25:25 +0300

    Add files via upload

---
+++ 6. Окна/6. Окна/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _6.Окна
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            foreach (Process process in Process.GetProcesses())
+            {
+                //Console.WriteLine($"Имя процесса: {process.ProcessName}");
+                c++;
+            }
+            var process1 = Process.GetCurrentProcess();
+            //Console.WriteLine(c);
+            //Console.WriteLine($"Name: {process1.ProcessName}");
+            Process proc = Process.GetProcessesByName("devenv")[0]; // 
+            ProcessModuleCollection modules = proc.Modules;
+            foreach (ProcessModule module in modules)
+            {
+                Console.WriteLine($"Name: {module.ModuleName}  FileName: {module.FileName}");
+            }
+
+        }
+    }
+}
