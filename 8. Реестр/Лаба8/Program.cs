2d9868511905fc701135d072f06d52a0b25912f1
Polina-Myatlik <121228435+Polina-Myatlik@users.noreply.github.com>
2023-04-27 23:11:19 +0300

    Add files via upload

---
+++ 8. Реестр/Лаба8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Win32;
+
+namespace Лаба8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\наша программа");
+            key.SetValue("Параметр", "Значение");
+            key.SetValue("Параметр2", "Значение2");
+            foreach (string n in key.GetValueNames())
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine();
+
+            key.SetValue("Параметр3", "Значение3");
+            Console.WriteLine(key?.GetValue("Параметр3")?.ToString());
+
+            Registry.Export(@"C:\registry_backup.reg");
+            Registry(@"C:\registry_backup.reg");
+
+            key.DeleteSubKey("SOFTWARE\\наша программа");
+            key.Close();
+        }
+    }
+}
