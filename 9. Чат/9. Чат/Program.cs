b28c5738650d630136f38aeefdd56d8e2b228bbd
Polina-Myatlik <121228435+Polina-Myatlik@users.noreply.github.com>
2023-05-08 00:35:51 +0300

    Add files via upload

---
+++ 9. Чат/9. Чат/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace _9.Чат
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(5); //выстраиваем очередь из 5
+
+            while (true)
+            {
+                var listener = tcpSocket.Accept(); //обрабатываем этого клиента
+                var buffer = new byte[256]; //созраняем данные сюда, хранилище данных
+                var size = 0;
+                var data = new StringBuilder(); //собирает полученные данные
+
+                do
+                {
+                    size = listener.Receive(buffer); //возвращаем значения
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size)); //перекодируем значения в строку
+                }
+                while (listener.Available > 0);
+
+                Console.WriteLine(data);
+
+                listener.Send(Encoding.UTF8.GetBytes("Успех"));
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+        }
+    }
+}
---
+++ 9. Чат/9.2 Клиент/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._2_Клиент
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Введите сообщение:");
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message); //получили данные
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data); //отправили сообщение
+
+            var buffer = new byte[256]; //созраняем данные сюда, хранилище данных
+            var size = 0;
+            var answer = new StringBuilder(); //собирает полученные данные
+
+            do
+            {
+                size = tcpSocket.Receive(buffer); //возвращаем значения
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size)); //перекодируем значения в строку
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
